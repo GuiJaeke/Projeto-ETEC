cc5c13d0379def7ef7715666e4396a3ffbcd2083
Gui Jaeke <107638175+GuiJaeke@users.noreply.github.com>
2024-07-16 13:25:03 -0300

    alterações script.js

--- public_html/script.js
+++ public_html/script.js
@@ -1,15 +1,14 @@
 // Seleciona o elemento com o id 'toggleMenu' e adiciona um evento de clique
 document.getElementById('toggleMenu').addEventListener('click', function() {
-    // Obtém a referência ao elemento do menu pelo id 'menu'
-    var menu = document.getElementById('menu');
+    var menu = document.getElementById('menu'); // Obtém a referência ao elemento do menu pelo id 'menu'
 
     // Verifica se o estilo display atual do menu é 'none'
-    if (menu.style.display == 'none') {
-        // Se o menu estiver oculto ('none'), muda o estilo display para 'block' (visível)
-        menu.style.display = 'block';
-    } else {
-        // Caso contrário, se o menu estiver visível (qualquer coisa diferente de 'none'),
-        // muda o estilo display para 'none' para ocultá-lo
-        menu.style.display = 'none';
+    if (menu.style.display == 'none') {        
+
+        menu.style.display = 'grid'; // Se o menu estiver oculto ('none'), muda o estilo display para 'block' (visível)
+    }
+    else { // Caso contrário, se o menu estiver visível (qualquer coisa diferente de 'none'),
+
+        menu.style.display = 'none'; // muda o estilo display para 'none' para ocultá-lo
     }
 });
